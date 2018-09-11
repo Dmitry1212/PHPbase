584d596ab97b2df71edea27ae81970f6bfbf98cb
Dmitry <shamaev.dmitry@yandex.ru>
2018-09-12 01:04:51 +0300

    домашнее задание 1

---
+++ lesson1.php
@@ -0,0 +1,13 @@
+<?php
+$new_url = 'http://lesson1/template.php';
+header('Location: '.$new_url);
+header("Content-type: text/html; charset=utf-8");
+
+
+?>
+<!--/**-->
+<!-- * Created by PhpStorm.-->
+<!-- * User: ДмитрийАТЕС-->
+<!-- * Date: 09.09.2018-->
+<!-- * Time: 3:30-->
+<!-- */-->
