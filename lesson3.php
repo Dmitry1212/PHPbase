680dbdbba9deb41fe83af586cc1776be6a01d321
Dmitry <shamaev.dmitry@yandex.ru>
2018-09-16 04:01:13 +0300

    доделано ДЗ №3

--- lesson3.php
+++ lesson3.php
@@ -60,30 +60,17 @@ foreach ($citiesByRegion as $region => $cities) {
     echo "<br>";
 }
 
-//8. *Повторить третье задание, но вывести на экран только города, начинающиеся с буквы «К».
-$letter = "К";
-foreach ($citiesByRegion as $region => $cities) {
-    echo "Субъект РФ: " . $region . ". Города на букву $letter: <br>";
-    foreach ($cities as $key => $city) {
-        //mb_substr($city,0,1,'UTF-8');
-        if (mb_substr($city, 0, 1, 'UTF-8') == $letter) {
-            echo(" Город $city <br/>");
-        }
-    }
-    echo "<br>";
-}
-
 
 //4. Объявить массив, индексами которого являются буквы русского языка, а значениями – соответствующие
 //латинские буквосочетания (‘а’=> ’a’, ‘б’ => ‘b’, ‘в’ => ‘v’, ‘г’ => ‘g’, …, ‘э’ => ‘e’, ‘ю’ => ‘yu’, ‘я’ => ‘ya’).
 
 $translation = [
-    'а'=> 'a',
+    'а' => 'a',
     'б' => 'b',
     'в' => 'v',
     'г' => 'g',
     'д' => 'd',
-    'е' => 'ye',
+    'е' => 'e',
     'ё' => 'yo',
     'ж' => 'j',
     'з' => 'z',
@@ -104,43 +91,119 @@ $translation = [
     'ц' => 'c',
     'ч' => 'ch',
     'ш' => 'sh',
-    'щ' => 'csh',
+    'щ' => 'sch',
+    'ь' => '',
+    'ъ' => '',
+    'ы' => 'i',
     'э' => 'e',
     'ю' => 'yu',
     'я' => 'ya',];
 
 
-function translation ($word, $translation) {
+function translation($word, $translation)
+{
     $length = strlen(utf8_decode($word));
-    $newString ='';
+    $newString = '';
     $word = mb_strtolower($word, 'UTF-8');
-    for ($i=0;$i<$length; $i++) {
-        $newString .= $translation[mb_substr($word, $i, 1, 'UTF-8')];
+    for ($i = 0; $i < $length; $i++) {
+        $letter = mb_substr($word, $i, 1, 'UTF-8');
+        if (array_key_exists($letter, $translation)) {
+            $newString .= $translation[$letter];
+        } else {
+            $newString .= $letter;
+        }
     }
     return $newString;
 }
 
 echo "<br>";
-$tesrWord ="Привет";
-echo "Слово на русском '$tesrWord' транслитерировано так: ". translation($tesrWord, $translation);
+$tesrWord = "Привет";
+echo "Слово на русском '$tesrWord' транслитерировано так: " . translation($tesrWord, $translation);
 echo "<br><br>";
 
 //5. Написать функцию, которая заменяет в строке пробелы на подчеркивания и возвращает видоизмененную строчку.
 
-$sayPhrase = str_replace (" ", "_", $sayPhrase);
-echo "<p> $sayPhrase</p>";
+function underscore($string)
+{
+    return str_replace(" ", "_", $string);
+}
+
+echo underscore("Необходимо представить пункты меню как элементы массива и вывести их циклом.");
+echo "<br><br>";
 
 //6. В имеющемся шаблоне сайта заменить статичное меню (ul – li) на генерируемое через PHP.
 //Необходимо представить пункты меню как элементы массива и вывести их циклом.
 //Подумать, как можно реализовать меню с вложенными подменю? Попробовать его реализовать.
 
+$mainMenu = [
+    'главная',
+    'тряпки мужские' => ['брюки', 'пиджаки', 'шляпы'],
+    'тряпки жеские' => [
+        'платьишко' => [
+            'вечернее',
+            'повседневное',
+        ],
+        'шляпка',
+    ],
+    'детские тряпки' => ['чепчики', 'штанишки'],
+    'контакты',
+];
+
+function buildMenu($menuArray)
+{
+    $submenu = "<ul>";
+    foreach ($menuArray as $key => $menu) {
+        if (is_array($menu)) {
+            $submenu .= "<li>" . $key . buildMenu($menu) . "</li>";
+        } else {
+            $submenu .= "<li>" . $menu . "</li>";
+        }
+    }
+    $submenu .= "</ul>";
+    return $submenu;
+}
+
+
+echo buildMenu($mainMenu);
+
+echo "<br><br>";
 
 //7. *Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. Выглядеть должно так:
 //for (…){ // здесь пусто}
 
-for ($i=0;$i<10; (print  $i++))
-{ // здесь пусто
+for ($i = 0; $i < 10; (print  $i++)) { // здесь пусто
+}
+
+echo "<br><br>";
+
+//8. *Повторить третье задание, но вывести на экран только города, начинающиеся с буквы «К».
+$letter = "К";
+foreach ($citiesByRegion as $region => $cities) {
+    echo "Субъект РФ: " . $region . ". Города на букву $letter: <br>";
+    foreach ($cities as $key => $city) {
+        //mb_substr($city,0,1,'UTF-8');
+        if (mb_substr($city, 0, 1, 'UTF-8') == $letter) {
+            echo(" Город $city <br/>");
+        }
     }
+    echo "<br>";
+}
+
+
+//9. *Объединить две ранее написанные функции в одну, которая получает строку на русском языке,
+//производит транслитерацию и замену пробелов на подчеркивания
+//(аналогичная задача решается при конструировании url-адресов на основе названия статьи в блогах).
+
+function makeUrl($string, $translation)
+{
+    $newString = '';
+    $newString = translation($string, $translation);
+    $newString = underscore($newString);
+    return $newString;
+}
+
+echo makeUrl("Необходимо представить пункты меню", $translation);
+echo "<br><br>";
 
 
 /**
