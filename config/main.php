7812eaee292f3bc2964d87ecfe61412b1e21b071
Dmitry <shamaev.dmitry@yandex.ru>
2018-09-27 14:22:37 +0300

    HW l4

--- config/main.php
+++ config/main.php
@@ -2,3 +2,4 @@
 define("DOCUMENT_ROOT", __DIR__ . "/../");
 define("ENGINE_DIR", DOCUMENT_ROOT . "engine/");
 define("UPLOADS_DIR", DOCUMENT_ROOT . "uploads/");
+define("UPLOADS_MIN_DIR", DOCUMENT_ROOT . "uploads/min/");
